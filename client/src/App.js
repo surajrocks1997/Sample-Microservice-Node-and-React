da639bb016abe1da21fad897c51727a98ddf9313
surajrocks1997 <surajrocks1997@gmail.com>
2020-05-31 23:04:25 +0530

    postcreation done and added cors library

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+import PostCreate from "./PostCreate";
+
 export default () => {
-  return <div>Sample Microservice App</div>;
+  return (
+    <div className='container'>
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  );
 };
