1b1d7fd43601f892dcf3beece8b6f2c5c6fbabb9
surajrocks1997 <surajrocks1997@gmail.com>
2020-06-04 12:19:37 +0530

    events being recieved on posts and comments

--- comments/index.js
+++ comments/index.js
@@ -36,4 +36,10 @@ app.post("/posts/:id/comments", async (req, res) => {
   res.status(201).send(comments);
 });
 
+app.post('/events', (req, res) => {
+  console.log('Recieved Event: ', req.body.type);
+
+  res.send({});
+})
+
 app.listen(4001, () => console.log("Listening on port 4001"));
