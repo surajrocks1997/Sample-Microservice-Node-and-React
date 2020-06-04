60e313bc7211271e9e431e9149891977de8145c1
surajrocks1997 <surajrocks1997@gmail.com>
2020-06-04 17:29:35 +0530

    handling moderation

--- moderation/index.js
+++ moderation/index.js
@@ -5,6 +5,23 @@ const app = express();
 
 app.use(express.json({ extended: false }));
 
-app.post("/events", (req, res) => {});
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const status = data.content.contains("orange") ? "rejected" : "approved";
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+  res.send({});
+});
 
 app.listen(4003, () => console.log("Listening on Port 4003"));
