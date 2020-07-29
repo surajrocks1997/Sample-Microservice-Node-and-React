5843d830b618b11093672278ca38d3a550b36d13
surajrocks1997 <surajrocks1997@gmail.com>
2020-07-29 13:34:09 +0530

    posts, comments and client added

---
+++ posts/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+
+const app = express();
+
+app.use(express.json({ extended: false }));
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => console.log("Listing on Port 4000"));
