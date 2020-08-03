7b2e25cf3e66a4015afceb76ba0e5a5031a4ba9d
surajrocks1997 <surajrocks1997@gmail.com>
2020-08-03 18:21:56 +0530

    data query service added

---
+++ query/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json({ extended: false }));
+app.use(cors());
+
+app.get("/posts", (req, res) => {
+
+})
+
+app.post("/events", (req, res) => {
+
+})
+
+app.listen(4002, () => console.log("Listening on Port 4002"))
